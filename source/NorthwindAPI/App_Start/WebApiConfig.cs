695b286efc30cfba51f688f1366b2a5207859bb9
Ram Gopinathan <rprakashg@outlook.com>
2015-06-01 17:13:35 -0700

    Additional files

---
+++ source/NorthwindAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+using Newtonsoft.Json.Serialization;
+
+namespace NorthwindAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().FirstOrDefault();
+            if(jsonFormatter != null)
+                jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            //remove xml media type formatter
+            var xmlFormatter = config.Formatters.OfType<XmlMediaTypeFormatter>().FirstOrDefault();
+            if (xmlFormatter != null)
+                config.Formatters.Remove(xmlFormatter);
+        }
+    }
+}
